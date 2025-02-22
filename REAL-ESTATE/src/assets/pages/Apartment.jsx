146bbac4dbe7b0f3cdf0f1d28376ce29484de30c
SajaniP <sajani.p@knightowl.online>
2025-02-22 21:39:43 +0530

    apartment header

--- REAL-ESTATE/src/assets/pages/Apartment.jsx
+++ REAL-ESTATE/src/assets/pages/Apartment.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/header';
+import Header from '../components/Header';
 import SearchForm from './info/SearchForm';
 import PropertyList from './info/PropertyList';
 import ImageGallery from './info/ImageGallery';
